27e9a3ffb474d216e4f3e42f7fbf8e133655135f
thisKK <42506615+thisKK@users.noreply.github.com>
2021-01-04 21:15:48 +0700

    add search api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./assets/logo.svg";
+import "./App.css";
+import Axios from 'axios'
+import MovieItem from "./components/movieItem";
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {rows:[]}
+  }
+  componentDidMount(){
+    this.search('Hello')
+  }
+  search  = (keyword) => {
+    var dataArray = []
+    var url = "https://api.themoviedb.org/3/search/movie?api_key=f5b1831414dc97da0f1eaa6087eaea29&query=" + keyword
+    
+    Axios.get(url).then(result=>{
+      result.data.results.forEach(item => {
+        item.poster_src = "https://image.tmdb.org/t/p/w185" + item.poster_path
+        dataArray.push(item)
+        console.log(dataArray)
+      })
+      this.setState({rows: dataArray})
+    })
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <table className="Navbar">
+          <tbody>
+            <tr>
+              <td>
+                <img src={logo} alt="logo" width="50" />
+              </td>
+              <td>hello world</td>
+            </tr>
+          </tbody>
+        </table>
+        <input
+          style={{
+            fontSize: 24,
+            display: "block",
+            width: "100%",
+            paddingLeft: 8,
+          }}
+          placeholder="Enter your movie name"
+          onChange={(event) => { this.search(event.target.value)
+          }}/>
+        {this.state.rows.map(item=>(
+        <MovieItem movie={item} />
+        )
+        ) }
+
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/movieItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class movieItem extends Component {
+    constructor(props){
+        super(props)
+    }
+    render() {
+        const {id ,title , overview,poster_src} = this.props.movie
+        return (
+            <table key={id} style = {{textAlign: 'left'}}>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src = {poster_src}/>
+                        </td>
+                        <td> 
+                            <strong> {title}</strong>
+                            <p>{overview}</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        )
+    }
+}
